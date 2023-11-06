dbb5111f751a560c10938e74eab32fc42b589da9
maheshbasnet089 <basnetmanish089@gmail.com>
2023-11-06 21:09:52 +0545

    first commit

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,39 @@
+<!-- <script setup>
+
+
+defineProps({
+  msg: String,
+})
+
+
+</script> -->
+
+
+
+
+<template>
+
+<div>
+  <RenderHelloWorld />
+</div>
+ 
+</template>
+<script>
+import {h} from 'vue'
+export default{
+
+  components : {
+    RenderHelloWorld : {
+      render(){
+        return h('h1','Hello i am Manish')
+      }
+    }
+  }
+}
+
+</script>
+<style scoped>
+.read-the-docs {
+  color: #888;
+}
+</style>
